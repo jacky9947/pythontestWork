f84dcba5154c0d22e8ef6a14d875556fe819426a
zhaocheng <5410121@qq.com>
2020-05-11 02:44:20 +0800

    first commit

---
+++ demo.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from python.demo1 import demo1
+from testing.demo2 import demo2
+#111122
+demo1()
+demo2()
---
+++ testing/demo2.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+class demo2:
+    pass
